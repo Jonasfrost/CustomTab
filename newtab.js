6584cd67a0a62183d7c204e49dd39542ce5710e4
Jonas Frost <jonasmfrost@gmail.com>
2026-08-24 14:11:18 +0200

    safhufp

--- newtab.js
+++ newtab.js
@@ -8,7 +8,7 @@ function initClock() {
     const startTime = new Date('2008-09-24T00:00:00');
     const greetingEl = document.getElementById("greeting");
     const clockEl = document.getElementById("clock");
-    const timeSinceEl = document.getElementById("timeSince");
+    const timeSinceEl = document.getElementById("simon-age");
 
     function update() {
         const now = new Date();
@@ -18,14 +18,20 @@ function initClock() {
         }
 
         const forskelMs = now - startTime;
-        const sec = Math.floor(forskelMs / 1000);
-        const min = Math.floor(sec / 60);
-        const hour = Math.floor(min / 60);
-        const day = Math.floor(hour / 24);
-        const year = Math.floor(day / 365.25);
+
+        let totalSec = Math.floor(forskelMs / 1000);
+        let totalMin = Math.floor(totalSec / 60);
+        let totalTimer = Math.floor(totalMin / 60);
+        let totalDage = Math.floor(totalTimer / 24);
+        let year = Math.floor(totalDage / 365.25);
+
+        let sec = totalSec % 60;
+        let min = totalMin % 60;
+        let timer = totalTimer % 24;
+        let day = totalDage % 365; 
 
         if (timeSinceEl) {
-            timeSinceEl.innerText = `${year} År,  ${day} Dage,  ${hour} Timer,  ${min} Minuter,  ${sec} Sekunder `;
+            timeSinceEl.innerText = `${year} År, ${day} Dage, ${timer} Timer, ${min} Minutter, ${sec} Sekunder`;
         }
 
         const hours = now.getHours();
@@ -37,6 +43,7 @@ function initClock() {
         else if (hours <= 18) greetingEl.innerText = "Damn";
         else greetingEl.innerText = "idk";
     }
+
     update();
     setInterval(update, 1000);
 }
@@ -49,7 +56,7 @@ async function fetchWeather() {
         const res = await fetch("https://api.open-meteo.com/v1/forecast?latitude=55.6761&longitude=12.5683&current_weather=true");
         const data = await res.json();
         const temp = data.current_weather.temperature;
-        weatherEl.innerHTML = `Copenhagen<br><strong>${temp - 1.7}°C</strong>`;
+        weatherEl.innerHTML = `Copenhagen<br><strong>${temp}°C</strong>`;
     } catch (err) {
         weatherEl.innerText = "Could not load weather.";
     }
@@ -58,8 +65,6 @@ async function fetchWeather() {
 function initTodo() {
     const input = document.getElementById("task-input");
     const addBtn = document.getElementById("add-task-btn");
-    const openBtn = document.getElementById("open-task-btn");
-    const taskModal = document.getElementById("task-modal");
     const list = document.getElementById("task-list");
 
     chrome.storage.local.get(["tasks"], (result) => {
@@ -79,10 +84,6 @@ function initTodo() {
                 input.value = "";
             });
         });
-    });
-
-    openBtn.addEventListener("click", () => {
-        taskModal.classList.toggle("active");
     });
 
     function renderTasks(tasks) {
